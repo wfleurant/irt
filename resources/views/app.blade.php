2d726cf1b2df00ebbff9986a97342ae634bfdc35
Daniel Supernault <danielsupernault@gmail.com>
2015-08-15 16:55:53 -0600

    Added base blade template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A modern self-hosted bash.org clone.">
+    <meta name="author" content="github.com/dansup/irt">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>irt | irc remembers that</title>
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    @yield('content')
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">IRT is <a href="https://github.com/dansup/irt">open source</a> MIT licenced software.</p>
+      </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  </body>
+</html>
